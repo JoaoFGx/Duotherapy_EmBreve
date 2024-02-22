78b5f0ab751c20b4984b61ee7ed877c6cae938fd
Joao Fernandes <joao.fdias789@gmail.com>
2024-02-21 23:17:09 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+      ],
+  theme: {
+    extend: {
+        colors: {
+            primarycolor: '#FFFFFF',
+            secondarycolor: '#FF0000',
+        },
+    },
+  },
+  plugins: [],
+}
+
