0012a5d1e301608867c0c542c67bc3ef9a6e3059
Joao Fernandes <joao.fdias789@gmail.com>
2024-03-06 15:10:54 -0300

    alteração color tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,8 @@ module.exports = {
   theme: {
     extend: {
         colors: {
-            primarycolor: '#B3B47A',
-            secondarycolor: '#2F5F37',
+            primarycolor: '#768d59',
+            secondarycolor: '#768d59',
         },
     },
   },
